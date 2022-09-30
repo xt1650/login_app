1e8b4dabd84bbfdc6014e55a95fbf9e0b5e7f2ef
havanur <simsekhavanur@gmail.com>
2022-09-30 14:31:26 +0300

    frontend guncelleme

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 from flask import Flask,jsonify, request,render_template
 import json
 import webview
+from foundation.db_controller import check, create_user, del_user, update_user, get_user_info
 
 
 app = Flask(__name__,static_folder='./assets', template_folder='./templates')
@@ -16,13 +17,15 @@ class Api:
         }
         print(self.response)
 
-    def kayitOlPost(self,kullanici,eposta,sifre):
+    def kayitOlPost(self,eposta,sifre):
         self.response = {
-            'kullaniciadi':kullanici,
-            'eposta':eposta,
-            'sifre' : sifre
+            'email':eposta,
+            'password' : sifre
         }
-        print(self.response)
+        
+        print(create_user(self.response['email'],self.response))
+
+        
 
 @app.route("/kayit",methods=['POST', 'GET'])
 def kayitol():
@@ -31,12 +34,15 @@ def kayitol():
 @app.route("/",methods=['POST', 'GET'])
 def index():
     return render_template('index.html')
+
    
 
 
 if __name__=='__main__':
     api = Api()
+    
     window = webview.create_window('PyWebView & Flask', app,js_api=api)
+
     webview.start()    
 
 
